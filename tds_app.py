38c92ecb3ed75b4b36dd94ed7227bf89559f5475
Christoph Jansen <Christoph.Jansen@htw-berlin.de>
2017-07-07 17:32:52 +0200

    bug fix

--- tds_app.py
+++ tds_app.py
@@ -5,7 +5,7 @@ from subprocess import call
 command = [
     'bash', '/root/run_sn_TDS.sh', '/usr/local/MATLAB/MATLAB_Runtime/v85',
     'data', '/root/input_files/psg.edf',
-    'montage_filename', '/root/montage.txt',
+    'montage_filename', '/root/input_files/montage.txt',
     'resultpath', '/root/result_files'
 ]
 
